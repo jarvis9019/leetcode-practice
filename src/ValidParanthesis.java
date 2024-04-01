76bb87dc2aa24e06575ecb81eb7c2c1f74fa1551
jarvis9019 <60092292+jarvis9019@users.noreply.github.com>
2024-04-01 23:01:50 +0530

    add : longest common prefix

---
+++ src/ValidParanthesis.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidParanthesis {
+    public boolean isValid(String s) {
+
+        if(s.length() == 0){
+            return false;
+        }
+
+        Map<String,String> checkMap = new HashMap<>();
+        checkMap.put("(",")");
+        checkMap.put("[","]");
+        checkMap.put("{","}");
+
+
+
+    }
+}
